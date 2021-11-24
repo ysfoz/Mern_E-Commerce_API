d10e17643aa3bf4649f404e71e4894fc6becd910
yusuf <ysfozturk3@gmail.com>
2021-11-24 22:03:48 +0100

    completed cart crud processes

---
+++ controller/cartController.js
@@ -0,0 +1,54 @@
+const Cart = require("../models/Cart");
+
+exports.createCart = async (req, res) => {
+  const cart = new Cart(req.body);
+  try {
+    const savedCart = await cart.save();
+    res.status(200).json(savedcart);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+exports.updataCart = async (req, res) => {
+  try {
+    const updetedCart = await Cart.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedCart);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+exports.deleteCart = async(req,res) => {
+    try {
+        await Cart.findByIdAndDelete(req.params.id)
+        res.status(200).json("Cart has been deleted")
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
+
+exports.getCart = async(req,res) => {
+    try {
+        const cart = await Cart.findOne({ userId : req.params.userId})
+        res.status(200).json(cart)
+        
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
+
+exports.getAllCarts = async(req,res) => {
+    try {
+        const allCarts = await Cart.find()
+        res.status(200).json(allCarts)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
---
+++ routes/cart.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const { verifyToken, verifyTokenAndAuthorization } = require("../middleware/verifyToken");
+
+const { createCart,updateCart, deleteCart, getCart, getAllCarts } = require("../controller/cartController");
+
+router.post("/", verifyToken, createCart);
+router.put("/:id", verifyTokenAndAuthorization,updateCart)
+router.delete("/:id", verifyTokenAndAuthorization, deleteCart)
+router.get("find/:userId",verifyTokenAndAuthorization, getCart)
+router.get("/",verifyTokenAndAuthorization, getAllCarts)
