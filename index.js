69f715f60f38517354788371ab3b520c4244a42b
yusuf <ysfozturk3@gmail.com>
2021-11-25 15:52:19 +0100

    added stripe router

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart")
 const orderRoute = require("./routes/order")
+const stripeRoute = require("./routes/stripe")
+const cors = require("cors")
 
 
 const app = express();
@@ -16,13 +18,14 @@ mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("connected MongoDB"))
   .catch((err) => console.log(err));
-
+app.use(cors())
 app.use(express.json());
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute)
 app.use("/api/orders", orderRoute)
+app.use("/api/checkout", stripeRoute)
 
 
 
--- routes/stripe.js
+++ routes/stripe.js
@@ -1,6 +1,7 @@
 const router= require("express").Router()
+const{ makePayment } = require("../controller/stripeController")
 
-
+router.post("/payment", makePayment)
 
 
 module.exports = router;
