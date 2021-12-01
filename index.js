c4ea42f37a2ee93cbb1e34faed023cb27ce736a5
yusuf <ysfozturk3@gmail.com>
2021-12-01 22:38:59 +0100

    added img to user model

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
+const dotenv = require("dotenv")
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
@@ -9,9 +9,10 @@ const orderRoute = require("./routes/order");
 const stripeRoute = require("./routes/stripe");
 const cors = require("cors");
 
+dotenv.config()
+
 const app = express();
 
-dotenv.config();
 
 mongoose
   .connect(process.env.MONGO_URL)
@@ -24,7 +25,7 @@ app.use("/api/auth", authRoute);
 app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
-app.use("/api/checkout", stripeRoute);
+app.use("/api/checkout",stripeRoute);
 
 app.listen(process.env.PORT || 5001, () => {
   console.log("server is running");
