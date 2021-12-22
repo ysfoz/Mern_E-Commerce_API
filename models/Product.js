2942318e1de196f67690c35b3307719d93218be9
yusuf <ysfozturk3@gmail.com>
2021-12-22 21:07:58 +0100

    added salesAmount to product model

--- models/Product.js
+++ models/Product.js
@@ -11,6 +11,7 @@ const ProductSchema = new mongoose.Schema(
     price: { type: Number, required: true },
     inStock: { type: Boolean, default: true },
     inSale: { type: Boolean, default: false },
+    salesAmount:{type:Number,default:0}
   },
   { timestamps: true }
 );
