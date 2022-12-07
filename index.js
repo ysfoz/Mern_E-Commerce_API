bb236f4e111d3b47ee8fa0c3cce3e621d735f185
ysfoz <ysfozturk3@gmail.com>
2022-12-07 14:55:58 +0100

    added Dockerfile

--- index.js
+++ index.js
@@ -26,7 +26,6 @@ app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
 app.use("/api/checkout",stripeRoute);
-app.use(express.static('/public'))
 
 const PORT = process.env.PORT || 8081
 
